b95834e5a7889b1c7ac954c3b3c9c084effcff61
Julien Ferrier <jferrier@localhost.localdomain>
2018-10-10 12:05:10 +0200

    git tasktwo_tree

--- my_strcat.c
+++ my_strcat.c
@@ -5,6 +5,24 @@
 ** Task02
 */
 
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int  my_putstr(char const *str)
+{
+    char i = *str;
+
+    while (i != '\0') {
+        i = *str;
+        my_putchar(i);
+        str = str + 1;
+    }
+    
+    return (0);
+}
+
 char *my_strcat(char *dest, char const *src)
 {
     int i = 0;
@@ -15,8 +33,15 @@ char *my_strcat(char *dest, char const *src)
     }
     while (src[j] != '\0') {
 	dest[i] = src[j];
-	    i++;
+	i++;
 	j++;
     }
-    return (1);
+    return (dest);
+}
+
+int main(void)
+{
+    char dest[10] = "bon";
+    my_putstr(my_strcat(dest, "jour"));
+    return (0);
 }
---
+++ my_strncat.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2018
+** my_strncat.c
+** File description:
+** Task03
+*/
+
+int my_strlen(char *dest)
+{
+    int n = 0;
+    
+    while (dest[n] != '\0') {
+        n = n + 1;
+    }
+    return (n);
+}
+
+char *my_strncat(char *dest, char const *src, int n)
+{
+    int i = 0;
+    int j = 0;
+    int length = my_strlen(dest);
+    
+    while (dest[i] != '\0') {
+        i++;
+    }
+    while (i < (n + length)) {
+        if (src[j] == '\0')
+            dest[i] = '\0';
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    return (dest);
+}
