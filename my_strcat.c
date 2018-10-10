97941a29dd8d80d3cc7b1aa6b3c1403ebc32384b
Julien Ferrier <jferrier@localhost.localdomain>
2018-10-10 21:29:29 +0200

    modified version task 1 /erreur main and my_putchar

--- my_strcat.c
+++ my_strcat.c
@@ -5,43 +5,18 @@
 ** Task02
 */
 
-void my_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
-int  my_putstr(char const *str)
-{
-    char i = *str;
-
-    while (i != '\0') {
-        i = *str;
-        my_putchar(i);
-        str = str + 1;
-    }
-    
-    return (0);
-}
-
 char *my_strcat(char *dest, char const *src)
 {
     int i = 0;
     int j = 0;
 
     while (dest[i] != '\0') {
-	i++;
+        i++;
     }
     while (src[j] != '\0') {
-	dest[i] = src[j];
-	i++;
-	j++;
+        dest[i] = src[j];
+        i++;
+        j++;
     }
     return (dest);
-}
-
-int main(void)
-{
-    char dest[10] = "bon";
-    my_putstr(my_strcat(dest, "jour"));
-    return (0);
 }
